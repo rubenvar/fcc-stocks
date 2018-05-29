8352c12014b9fd59e2adcc7f8ec5eeb510e06af7
Rubén <hey@rubenvara.io>
2018-05-29 10:19:34 +0200

    first api connection

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,3 +1,16 @@
-exports.renderMain = (req, res) => {
-  res.render('main', { title: 'Main Page' });
+const axios = require('axios');
+
+exports.renderMain = async (req, res) => {
+  const symbols = 'aapl,fb,msft';
+  const uri = 'https://api.iextrading.com/1.0/stock/market/batch?symbols=' + symbols + '&types=quote,chart&range=1m&last=10';
+  let theData;
+  await axios
+    .get(uri)
+    .then(resp => {
+      theData = resp.data;
+    }).catch(error => {
+      console.log(error);
+    });
+    res.json(theData);
+  // res.render('main', { title: 'Main Page' });
 }
